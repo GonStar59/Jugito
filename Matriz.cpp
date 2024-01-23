0271d3adf500d5fe1d3817e2c65c0e0384b11b14
GonStar59 <150295743+GonStar59@users.noreply.github.com>
2024-01-23 21:50:38 +0100

    Add files via upload

--- Matriz.cpp
+++ Matriz.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<random>
+#include <ctime>
 
 void PedirMatriz (int matriz[100][100],int n) 
 {
@@ -29,7 +31,33 @@ void SacarMatriz(int matriz[100][100],int n)
      std::cout<<std::endl;
 }
 
+void RandomMatriz(int M[100][100],int n){
+    for(i=0;i<n;i++)
+{
+    for(j=0;j<n;j++)
+    {
+       matriz[i][j]=std::rand() % (201) - 100;;
+    }
 
+}
+}
+
+void MatrizIdentidad(int Identidad[100][100],int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++)
+        {
+        if (i==j)
+        {
+            Identidad[i][j]=1;
+        }else{
+            Identidad[i][j]=0; 
+        }
+        
+        }
+    }
+
+}
 
 void SumaMatriz(int M1[100][100],int M2[100][100],int R[100][100],int n)
 {
@@ -93,7 +121,7 @@ void MultMatriz(int M1[100][100],int M2[100][100],int Resultado[100][100],int n)
 
 int main () 
 {
-int matriz1[100][100],matriz2[100][100],Resultado[100][100],op,n;
+int matriz1[100][100],matriz2[100][100],Resultado[100][100],Identidad[100][100],op,n,op2;
 
     std::cout<<"Tamaño de la matriz n*n, n="<<std::endl;
     std::cin>>n;
@@ -103,23 +131,73 @@ int matriz1[100][100],matriz2[100][100],Resultado[100][100],op,n;
     PedirMatriz(matriz2,n);
 
     do{
+    MatrizIdentidad(Identidad,n);
 
     SacarMatriz(matriz1,n);
 
     SacarMatriz(matriz2,n);
     
-    std::cout<<"Eliga la operacion de matrizes \n\t1) Suma A+B \n\t2) Resta A-B \n\t3) Multiplica A*B\n\t4) Multiplia A por otro numero \n\t5) Multiplia B por otro numero \n\t6) Cambia A \n\t7) Cambia B \n\t8) Cambia la dimension \n\t9) Cambia todo \n\t0) Salir";
+    std::cout<<"Eliga la operacion de matrizes \n\t1) Suma\n\t2) Resta\n\t3) Multiplicacion\n\t4) Multiplia A por otro numero \n\t5) Multiplia B por otro numero \n\t6) Cambia A \n\t7) Cambia B \n\t8) Cambiar todo \n\t9) Feeling Lucky\n\t0) Salir";
     std::cin>>op;
     if(op==1){
-        SumaMatriz(matriz1,matriz2,Resultado,n);
+         std::cout<<"Que quieres Sumar que:\n\t1)A+B\n\t2)A+Identidad\n\t3)B+Identidad\n"
+        std::cin>>op2;
+        if (op2==1) {
+            SumaMatriz(matriz1, matriz2, Resultado,n);
+            }
+            if (op2==3){
+            SumaMatriz(matriz1, Identidad, Resultado,n);
+            }
+            if (op2==5) {
+            SumaMatriz(matriz2, Identidad, Resultado,n);
+            }
         SacarMatriz(Resultado,n);
     }
     if(op==2){
-        RestaMatriz(matriz1,matriz2,Resultado,n);
+        std::cout<<"Que quieres Restar que:\n\t1)A-B\n\t2)B-A\n\t3)A-Identidad\n\t4)Identidad-A\n\t5)B-Identidad\n\t6)Identidad-B\n"
+        std::cin>>op2;
+        if (op2==1) {
+            RestaMatriz(matriz1, matriz2, Resultado,n);
+            }
+            if (op2==2) {
+            RestaMatriz(matriz2,matriz1, Resultado,n);
+            }
+            if (op2==3){
+            RestaMatriz(matriz1, Identidad, Resultado,n);
+            }
+            if (op2==4) {
+            RestaMatriz(Identidad, matriz1, Resultado,n);
+            }
+            if (op2==5) {
+            RestaMatriz(matriz2, Identidad, Resultado,n);
+            }
+            if (op2==6){
+            RestaMatriz(Identidad, matriz2, Resultado,n);
+            }
         SacarMatriz(Resultado,n);
     }
     if(op==3){
-        MultMatriz(matriz1, matriz2, Resultado,n);
+        std::cout<<"Que quieres Multiplicar:\n\t1)A*B\n\t2)B*A\n\t3)A*Identidad\n\t4)Identidad*A\n\t5)B*Identidad\n\t6)Identidad*B\n"
+        std::cin>>op2;
+            if (op2==1) {
+            MultMatriz(matriz1, matriz2, Resultado,n);
+            }
+            if (op2==2) {
+            MultMatriz(matriz2,matriz1, Resultado,n);
+            }
+            if (op2==3){
+            MultMatriz(matriz1, Identidad, Resultado,n);
+            }
+            if (op2==4) {
+            MultMatriz(Identidad, matriz1, Resultado,n);
+            }
+            if (op2==5) {
+            MultMatriz(matriz2, Identidad, Resultado,n);
+            }
+            if (op2==6){
+            MultMatriz(Identidad, matriz2, Resultado,n);
+            }
+            
         SacarMatriz(Resultado,n);
     }
      if(op==4){
@@ -141,13 +219,14 @@ int matriz1[100][100],matriz2[100][100],Resultado[100][100],op,n;
     if(op==8){
         std::cout<<"Tamaño de la matriz n*n, n="<<std::endl;
         std::cin>>n;
-    }
-    if(op==9){
-        std::cout<<"Tamaño de la matriz n*n, n="<<std::endl;
-        std::cin>>n;
         PedirMatriz(matriz1,n);
 
         PedirMatriz(matriz2,n);
+    if(op==9){
+        srand(time(0));
+        n=std::rand() % (101);
+        RandomMatriz(matriz1,n);
+        RandomMatriz(matriz2,n);
     }
     
 
